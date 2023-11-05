34fd9eb3f24bcf4ab5c43f52489d06a180d8e42b
vivekgupta96 <vivekg0691@gmail.com>
2023-11-05 11:41:41 +0530

    update

--- eCommersApp/src/main/java/Ecom/SecurityConfig/CorsConfig.java
+++ eCommersApp/src/main/java/Ecom/SecurityConfig/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-//        config.addAllowedOrigin("http://localhost:4000");
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedOrigin("*");
         config.addAllowedMethod("GET");
         config.addAllowedMethod("POST");
